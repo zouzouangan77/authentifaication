3124be4ba2b2a99edb9e51661b360149807736ab
zouzou77 <zouzou.angan@groupe-esigelec.org>
2025-04-13 02:14:33 +0200

    mise en place du domaine user

--- src/main/java/com/samuelangan/mycompagny/authentification/domain/User.java
+++ src/main/java/com/samuelangan/mycompagny/authentification/domain/User.java
@@ -1,9 +1,37 @@
 package com.samuelangan.mycompagny.authentification.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    private Integer id;
-    private String firName;
-    private String lastName;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    private boolean enabled;
+
+    @Column( nullable = false)
+    private String firName;
+
+    @Column( nullable = false)
+    private String lastName;
+
 }
