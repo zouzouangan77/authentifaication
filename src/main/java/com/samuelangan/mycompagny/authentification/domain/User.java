f72c024686ad70d9a39ff4730476207de6dc0eba
zouzou77 <zouzou.angan@groupe-esigelec.org>
2025-04-13 02:05:58 +0200

    ajout des elements de la structure du projet

---
+++ src/main/java/com/samuelangan/mycompagny/authentification/domain/User.java
@@ -0,0 +1,9 @@
+package com.samuelangan.mycompagny.authentification.domain;
+
+public class User {
+    private Integer id;
+    private String firName;
+    private String lastName;
+    private String email;
+    private String password;
+}
